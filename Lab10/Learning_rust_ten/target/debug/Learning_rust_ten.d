188cbdaa92e9e42c25a44ba6976a895bbef0b811
ironic833 <39494199+ironic833@users.noreply.github.com>
2023-05-20 23:51:00 +0100

    Updates
    - Added Lab10 Base

---
+++ Lab10/Learning_rust_ten/target/debug/Learning_rust_ten.d
@@ -0,0 +1,1 @@
+D:\Github\Learning-Rust\Lab10\Learning_rust_ten\target\debug\Learning_rust_ten.exe: D:\Github\Learning-Rust\Lab10\Learning_rust_ten\src\main.rs
---
+++ Lab10/Learning_rust_ten/target/debug/deps/Learning_rust_ten.d
@@ -0,0 +1,5 @@
+D:\Github\Learning-Rust\Lab10\Learning_rust_ten\target\debug\deps\Learning_rust_ten.exe: src\main.rs
+
+D:\Github\Learning-Rust\Lab10\Learning_rust_ten\target\debug\deps\Learning_rust_ten.d: src\main.rs
+
+src\main.rs:
