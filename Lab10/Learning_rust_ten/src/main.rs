acb62cb93f8185a3414e813ba11f7c01c99d8289
ironic833 <39494199+ironic833@users.noreply.github.com>
2023-06-26 22:27:34 +0100

    Lab 10
    - Added base files for lab 10

--- Lab10/Learning_rust_ten/src/main.rs
+++ Lab10/Learning_rust_ten/src/main.rs
@@ -1,3 +1,17 @@
+
+fn stack_function(mut var:i32) {
+    var = 56;
+    println!("Var: {}", var);
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let stack_num: i32 = 32;
+    let mut heap_vec: Vec<i32> = vec![4,5,6];
+
+    stack_function(stack_num);
+    println!("The value inside the main of stack_num: {}", stack_num);
+
 }
