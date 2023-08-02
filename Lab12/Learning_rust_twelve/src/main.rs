7fbabd764fa3f47e20a21b3fcde8833c46205579
ironic833 <39494199+ironic833@users.noreply.github.com>
2023-08-02 20:37:02 +0100

    Updates 12
    - Added more to lab 12

--- Lab12/Learning_rust_twelve/src/main.rs
+++ Lab12/Learning_rust_twelve/src/main.rs
@@ -1,3 +1,5 @@
+
+
 fn main() {
 
     let some_number = 40;
@@ -15,6 +17,51 @@ fn main() {
     }
 
     let flag_1 = true;
-    let flag_2 = true;
+    let flag_2 = false;
+
+    if flag_1 == true || flag_2 == true {
+        println!("One of the conditions is true here");
+    }
     
+    let flag_1 = false;
+
+    if flag_1 != true {
+        println!("Will execute when flag is not true");
+    }
+
+    let flag_1 = true;
+    let flag_2 = false;
+
+    let number = 60;
+
+    if (flag_1 == true && flag_2 == false) || number < 50 {
+        println!("Testing the conditions");
+    }
+
+    let marks = 80; 
+
+    if marks > 50 {
+        println!("You have passed");
+    } else {
+        println!("You have failed");
+    }
+
+    let marks = 95; 
+    let mut grade: char = 'N';
+
+
+    if marks >= 90 {
+        grade = 'A';
+    } else if marks >= 80 {
+        grade = 'B';
+    } else if marks >= 70 {
+        grade = 'C';
+    } else if marks >= 60 {
+        grade = 'D';
+    } else {
+        grade = 'F';
+    }
+
+    println!("The obtained grade is {}", grade);
+
 }
